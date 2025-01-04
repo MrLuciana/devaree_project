37edee7478d0658689ab8af06471ded7cbef6e7d
MrLuciana <mrbiiigs17@gmail.com>
2025-01-05 00:28:35 +0700

    update

--- backend/backend/includes/footer.php
+++ backend/backend/includes/footer.php
@@ -3,8 +3,8 @@
         <nav class="pull-left">
             <ul class="nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="http://www.themekita.com">
-                        ThemeKita
+                    <a class="nav-link" href="#">
+                        ThemeBB
                     </a>
                 </li>
                 <li class="nav-item">
@@ -16,8 +16,8 @@
             </ul>
         </nav>
         <div class="copyright">
-            2024, made with <i class="fa fa-heart heart text-danger"></i> by
-            <a href="http://www.themekita.com">ThemeKita</a>
+            2024, made with by
+            <a href="#">ThemeBB</a>
         </div>
         <div>
             Distributed by
