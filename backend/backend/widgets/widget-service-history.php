4a3a51331a76d90343d8866bad77202186d53bdf
bKoZii <bigkonkamon@gmail.com>
2025-01-08 21:33:34 +0700

    feat(dashboard): :sparkles: เพิ่มตารางการใช้งานบริการลูกค้า
    เพิ่มข้อมูล Demo ที่ตรงกับโปรเจค

---
+++ backend/backend/widgets/widget-service-history.php
@@ -0,0 +1,76 @@
+<div class="col">
+  <div class="card card-round">
+    <div class="card-header">
+      <div class="card-head-row card-tools-still-right">
+        <div class="card-title">ประวัติการใช้งานร้าน</div>
+        <div class="card-tools">
+          <div class="dropdown">
+            <button class="btn btn-icon btn-clean me-0" type="button" id="dropdownMenuButton"
+              data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-ellipsis-h"></i>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card-body p-0">
+      <div class="table-responsive">
+        <!-- Projects table -->
+        <table class="table align-items-center mb-0">
+          <thead class="thead-light">
+            <tr>
+              <th scope="col">รหัสคิว</th>
+              <th scope="col" class="text-end">ลูกค้า</th>
+              <th scope="col" class="text-end">วันที่ใช้บริการ</th>
+              <th scope="col" class="text-end">จำนวนเงิน</th>
+              <th scope="col" class="text-end">สถานะ</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">
+                <button class="btn btn-icon btn-round btn-success btn-sm me-2">
+                  <i class="fa fa-check"></i>
+                </button>
+                บริการ นวดแผนไทย #M0001
+              </th>
+              <td class="text-end">
+                <div class="d-flex align-items-center justify-content-end">
+                  <div class="avatar-sm me-2"><img class="avatar-img rounded-circle" src="https://res.cloudinary.com/dqx4sss9s/image/upload/v1724321379/me-rmutsv-square_hhiarv.webp" alt="กรกมล ศรีอ่อน"></div><span>กรกมล ศรีอ่อน</span>
+                </div>
+              </td>
+              <td class="text-end">Jan 08, 2025, 10.45am</td>
+              <td class="text-end">฿250.00</td>
+              <td class="text-end">
+                <span class="badge badge-success">สำเร็จ</span>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">
+                <button class="btn btn-icon btn-round btn-success btn-sm me-2">
+                  <i class="fa fa-check"></i>
+                </button>
+                บริการ นวดเท้า #M0002
+              </th>
+              <td class="text-end">
+                <div class="d-flex align-items-center justify-content-end">
+                  <div class="avatar-sm me-2"><img class="avatar-img rounded-circle" src="https://res.cloudinary.com/dqx4sss9s/image/upload/v1724321379/me-rmutsv-square_hhiarv.webp" alt="กรกมล ศรีอ่อน"></div><span>กรกมล ศรีอ่อน</span>
+                </div>
+              </td>
+              <td class="text-end">Jan 06, 2025, 10.05am</td>
+              <td class="text-end">฿500.00</td>
+              <td class="text-end">
+                <span class="badge badge-success">สำเร็จ</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
