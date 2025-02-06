765711c8c89c9010025db91325603a3755c8c18d
Konkamon Sion <bigkonkamon@gmail.com>
2025-02-06 20:01:21 +0700

    feat(Frontend): :sparkles: เพิ่มไฟล์ Bootstrap แบบ Custom

--- frontend/includes/head.php
+++ frontend/includes/head.php
@@ -2,6 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-custom.min.css">
     <link rel="stylesheet" href="assets/css/main.css">
     <script src="assets/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
