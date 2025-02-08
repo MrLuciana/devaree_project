a1053a35b1fa05a16d5fc5f82fd481d3fafee418
Konkamon Sion <bigkonkamon@gmail.com>
2025-02-08 16:07:04 +0700

    feat(Frontend): :sparkles: เพิ่มการแสดงข้อมูลของผู้ใช้แบบคร่าวๆ

--- frontend/pages/user.php
+++ frontend/pages/user.php
@@ -2,5 +2,14 @@
   <section class="container">
     <h1>หน้าข้อมูลผู้ใช้</h1>
     <p>แสดงรายละเอียดบัญชีผู้ใช้ / ประวัติการใช้บริการ</p>
+    <section class="card">
+      <div class="card-header">
+        <h4 class="m-0"> <?php echo htmlspecialchars($_SESSION['profile']->name); ?></h4>
+      </div>
+      <div class="card-body">
+        <p>ID: <?php echo htmlspecialchars($_SESSION['profile']->userId); ?></p>
+        <img src="<?php echo htmlspecialchars($_SESSION['profile']->picture); ?>" alt="Profile Picture" width="200px" class="rounded">
+      </div>
+    </section>
   </section>
 </body>
