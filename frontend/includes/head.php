212d7007799a2234b16e7f43a43f24460fcc52ff
Konkamon Sion <bigkonkamon@gmail.com>
2025-02-08 17:19:43 +0700

    chore(Frontend: Head): :sparkles: เพิ่ม CDN & ลบ link ที่ไม่ได้ใช้งาน
    เพิ่ม SweetAlert2 & Masonry Layout

--- frontend/includes/head.php
+++ frontend/includes/head.php
@@ -1,9 +1,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/bootstrap-custom.min.css">
     <link rel="stylesheet" href="assets/css/main.css">
     <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
 </head>
